a0355319e90082e43407489a141ee2f79157dcc4
henri pureza <henrique.pureza13@gmail.com>
2021-01-29 12:16:15 +1100

    Implemented remove from cart action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,8 +1,8 @@
 import Axios from "axios";
-import {CART_ADD_ITEM} from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
-  const {data} = await Axios.get(`/api/product/${productId}`);
+  const { data } = await Axios.get(`/api/product/${productId}`);
   dispatch({
     type: CART_ADD_ITEM,
     payload: {
@@ -14,5 +14,10 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
       qty,
     },
   });
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
-}
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (productId) => async (dispatch, getState) => {
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
